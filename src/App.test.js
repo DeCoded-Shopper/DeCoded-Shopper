61526e32928c4655162f4ae5483ec48786e9a976
tshavhalufu <singotv80@gmail.com>
2022-08-24 20:18:37 +0200

    testing the codecov percentage

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Is the link correct', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement.href).toContain('.org');
+});
