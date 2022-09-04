88670ea91ab47d93c14661b21ca30c60376f86a6
khanophidane <72083649+khanophidane@users.noreply.github.com>
2022-09-04 16:12:24 +0200

    navigation pages

---
+++ src/pages/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../index.css'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>dC Shopping 2022&copy;</p>
+    </div>
+  );
+};
+
+export default Footer;
