f5bfedf0a0e82c8c07d564227707651ebb8208ca
khanophidane <72083649+khanophidane@users.noreply.github.com>
2022-09-04 15:33:59 +0200

    navigation pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import Register from './Register';
-import Login from './Login';
-import { BrowserRouter as Router, Route,Routes} from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./pages/Footer";
+import Login from "./pages/Login";
+import Register  from "./pages/Register";
+import Products from "./pages/Products";
+import WishList  from "./pages/WishList";
+import "./index.css";
+
+import { Route, Routes } from "react-router-dom"
 
 function App() {
-
   return (
-    <main className="App">
-      <Login/>
-    </main>
-  );
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/wishlist" element={<WishList />} />
+          <Route path="/register" element={<Register/>}/>
+        </Routes>
+      </div>
+      <Footer/>
+    </>
+  )
 }
 
-export default App;
+export default App
