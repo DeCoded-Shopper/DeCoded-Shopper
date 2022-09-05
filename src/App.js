f0e1bceecfbc8ee56e13b0fa8276f35b7b34fdd4
Sizwe Sithole <2393358@students.wits.ac.za>
2022-09-05 16:19:29 +0200

    Attempting to add geolocation

--- src/App.js
+++ src/App.js
@@ -5,8 +5,45 @@ import Register  from "./pages/Register";
 import Products from "./pages/Products";
 import WishList  from "./pages/WishList";
 import "./index.css";
+import React, { Component } from "react";
 
 import { Route, Routes } from "react-router-dom"
+import { geolocated } from "react-geolocated";
+
+class App extends Component{
+  render(){
+        // Check geolocation supported in
+    // browser or not
+    return this.props.isGeolocationAvailable ? (
+ 
+      // Check location is enable in
+      // browser or not
+      this.props.isGeolocationEnabled ? (
+ 
+        // Check coordinates of current
+        // location is available or not
+        this.props.coords ? (
+          <div>
+            <h1 style={{ color: "green" }}>GeeksForGeeks</h1>
+            <h3 style={{ color: "red" }}>
+              Current latitude and longitude of the user is
+            </h3>
+            <ul>
+              <li>latitude - {this.props.coords.latitude}</li>
+              <li>longitude - {this.props.coords.longitude}</li>
+            </ul>
+          </div>
+        ) : (
+          <h1>Getting the location data</h1>
+        )
+      ) : (
+        <h1>Please enable location on your browser</h1>
+      )
+    ) : (
+      <h1>Please, update your or change the browser </h1>
+    );
+  }
+}
 
 function App() {
   return (
@@ -24,5 +61,4 @@ function App() {
     </>
   )
 }
-
-export default App
+export default geolocated()(App);
