bb7451c455359179d5a1d2a130822bc7f6d1f4e6
khanophidane <72083649+khanophidane@users.noreply.github.com>
2022-09-05 19:50:45 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,49 +1,15 @@
-import Navbar from "./Navbar";
+import Navbar from "./Navbar"
+import Pricing from "./pages/Pricing"
+import Home from "./pages/Home"
+import About from "./pages/About"
 import Footer from "./pages/Footer";
 import Login from "./pages/Login";
 import Register  from "./pages/Register";
 import Products from "./pages/Products";
 import WishList  from "./pages/WishList";
 import "./index.css";
-import React, { Component } from "react";
 
 import { Route, Routes } from "react-router-dom"
-import { geolocated } from "react-geolocated";
-
-class App extends Component{
-  render(){
-        // Check geolocation supported in
-    // browser or not
-    return this.props.isGeolocationAvailable ? (
- 
-      // Check location is enable in
-      // browser or not
-      this.props.isGeolocationEnabled ? (
- 
-        // Check coordinates of current
-        // location is available or not
-        this.props.coords ? (
-          <div>
-            <h1 style={{ color: "green" }}>GeeksForGeeks</h1>
-            <h3 style={{ color: "red" }}>
-              Current latitude and longitude of the user is
-            </h3>
-            <ul>
-              <li>latitude - {this.props.coords.latitude}</li>
-              <li>longitude - {this.props.coords.longitude}</li>
-            </ul>
-          </div>
-        ) : (
-          <h1>Getting the location data</h1>
-        )
-      ) : (
-        <h1>Please enable location on your browser</h1>
-      )
-    ) : (
-      <h1>Please, update your or change the browser </h1>
-    );
-  }
-}
 
 function App() {
   return (
@@ -52,6 +18,8 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<Products />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
           <Route path="/login" element={<Login />} />
           <Route path="/wishlist" element={<WishList />} />
           <Route path="/register" element={<Register/>}/>
@@ -61,4 +29,5 @@ function App() {
     </>
   )
 }
-export default geolocated()(App);
+
+export default App
