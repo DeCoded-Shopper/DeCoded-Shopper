68c0307a3bbf6580964fb23d611ff10c58867e27
tshavhalufu <singotv80@gmail.com>
2022-11-13 20:35:42 +0200

    last fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Products from "./components/Products";
-
 import WishList from "./components/WishList";
 import ProfilePage from "./components/ProfilePage";
 import { AuthProvider } from "./context/AuthProvider";
-
-import { CartProvider } from "react-use-cart";
 import "./index.css";
 
 import { Route, Routes } from "react-router-dom";
